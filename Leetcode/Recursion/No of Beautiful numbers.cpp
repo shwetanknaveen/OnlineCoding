1f5987ed83bd2dba882644780acf664276edb9af
shwetank singh <shwetanknaveen@gmail.com>
2021-09-08 02:14:34 +0530

    adobe test leetcode beautiful arrangements

---
+++ Leetcode/Recursion/No of Beautiful numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void getCount(vector<bool> &visited, int pos,int n, int &ans)
+{
+	if(pos>n)
+	{
+		ans++;//only those recursive call will reach the end which will have beautiful arrangement
+		return;
+	}
+	for(int i = 1;i<=n;i++)//i is the number
+	{
+		if(!visited[i] && ((i%pos == 0) || (pos%i ==0)))
+		{
+			visited[i] = true;//this i won't be taken down in the recursive call....see video for help
+			getCount(visited,pos+1,n,ans);
+			visited[i] = false;//this element is again free to be taken in another branch
+		}
+	}
+}
+
+int countArrangement(int n) {
+	
+	vector<bool> visited(n+1,false);//since we are taking here indexing from 1 so taking size as n+1
+	int ans = 0;
+	getCount(visited,1,n,ans);
+    return ans;    
+    }
+};
