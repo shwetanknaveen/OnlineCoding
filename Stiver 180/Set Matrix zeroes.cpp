6c6a70f58073221db8a57aded982efc4145ba7f3
shwetank singh <shwetanknaveen@gmail.com>
2021-11-13 13:32:12 +0530

    set matrix zeroes

---
+++ Stiver 180/Set Matrix zeroes.cpp
@@ -0,0 +1,31 @@
+//Problem link - https://leetcode.com/problems/set-matrix-zeroes/
+//Video link - https://www.youtube.com/watch?v=M65xBewcqcI&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=8
+//Time complexity - O(mn) Space complexity O(1)
+/*
+Setting of zeroes shouldn't be cascaded that is if because of one zero, a cell is set
+zero...then that cell shouldn't force other cells in its row and column to be zeroes
+*/
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+
+        //IMPORTANT - first column has to be dealt seperately with the flag col0
+        //why it is done see in the video at 8:50
+        
+    for (int i = 0; i < rows; i++) {
+        if (matrix[i][0] == 0) col0 = 0;//if there is 0 anywhere in first column then set col0 as 0
+        for (int j = 1; j < cols; j++)//first column is dealt seperately in above line
+            if (matrix[i][j] == 0)
+                matrix[i][0] = matrix[0][j] = 0;//flag the leftmost and topmost cell as 0
+    }
+
+    for (int i = rows - 1; i >= 0; i--) {//traverse from back
+        for (int j = cols - 1; j >= 1; j--)//till second column
+            if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                matrix[i][j] = 0;
+        if (col0 == 0) matrix[i][0] = 0;//first column is dealt seperately here
+		//if flag col0 is set then set the first column cell as 0
+    }
+    }
+};
