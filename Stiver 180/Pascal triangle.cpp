f8b281ad6d615c4b30b5c11efb55b318df2c6ea3
shwetank singh <shwetanknaveen@gmail.com>
2021-11-14 12:16:51 +0530

    changes in pascal triangle

--- Stiver 180/Pascal triangle.cpp
+++ Stiver 180/Pascal triangle.cpp
@@ -44,3 +44,27 @@ for rows = 5
 1 4 6 4 1
 */
 
+//Pascal value at r row and c column is (r-1)C(c-1)
+
+
+
+//Striver best solution
+/*
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> r(numRows);//give value for number of rows
+
+        for (int i = 0; i < numRows; i++) {
+            r[i].resize(i + 1);//no. of column in each row is row number and here indexing is from zero
+            r[i][0] = r[i][i] = 1;//first and last value is 1
+  
+            for (int j = 1; j < i; j++)
+                r[i][j] = r[i - 1][j - 1] + r[i - 1][j];//values from second column upto second last column is sum of value just above it and its left value
+        }
+        
+        return r;
+    }
+};
+*/
+
