4a1ab7c903e690cd47107cd9d5b2bd548dc43b32
shwetank singh <shwetanknaveen@gmail.com>
2021-11-22 16:09:24 +0530

    Pow(x,n)

---
+++ Stiver 180/Pow(x, n).cpp
@@ -0,0 +1,28 @@
+//Problem link - https://leetcode.com/problems/powx-n/submissions/
+//Video link - https://www.youtube.com/watch?v=l0YC3876qxg&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=16
+//Time complexity -> O(log(n)) where n is the power
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long nCopy = n ;//storing n in long long as when INT_MIN is converted into positive integer, it will cause overflow
+        double ans = 1.0;
+        if(n<0) nCopy = -nCopy;
+        
+        while(nCopy)
+        {
+            if(nCopy&1)//power is odd
+            {
+                ans = ans * x;
+                nCopy = nCopy-1;
+            }
+            else//power is even 
+            {
+                x = x * x;//note here that ans is not squared and x is squared and stored back into x
+                nCopy =nCopy/2;
+            }
+        }
+        if(n<0) ans = double(1.0)/ans;//if power was originally negative then we have to use
+                                        //x^(-)n = 1/(x^n) 
+        return ans;
+    }
+};
