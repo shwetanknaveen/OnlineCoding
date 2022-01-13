d09a8aea7d228e45d554e5d0741edb86a49e1f29
shwetank singh <shwetanknaveen@gmail.com>
2022-01-14 00:06:19 +0530

    Rat in a Maze Problem - I update

--- Stiver 180/Rat in a Maze Problem - I.cpp
+++ Stiver 180/Rat in a Maze Problem - I.cpp
@@ -1,6 +1,12 @@
 //Problem link - https://practice.geeksforgeeks.org/problems/rat-in-a-maze-problem/1#
 //Video link - https://www.youtube.com/watch?v=bLGZhJlt4y0&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=60
 
+/*
+Time Complexity: O(3^(n^2)). 
+As there are N^2 cells from each cell there are 3 unvisited neighbouring cells. So the time complexity O(3^(N^2).
+
+Space Complexity: O(n^2) ->All cells can be at stack at max
+*/
 class Solution{
     public:
     void solve(vector<vector<int>> &m,int &n,int i,int j,string &s,vector<string> &ans)
@@ -53,11 +59,45 @@ class Solution{
             return ans;
         }
         solve(m,n,0,0,s,ans);
-        if(!ans.empty()) return ans;//if ans is not empty it means at least one path is there to reach the destination
-        else//if ans hasn't got even a single path then it means no path was possible
-        {
-            ans.push_back("-1");
-            return ans;
+        return ans;
+    }
+};
+
+
+
+
+
+//Above code in a neat way as below
+
+class Solution{
+    void solve(int i, int j, vector<vector<int>> &a, int &n, vector<string> &ans, string &move, 
+     int di[], int dj[],string &dir) {
+        if(i==n-1 && j==n-1) {
+            ans.push_back(move);
+            return; 
         }
+         
+        for(int ind = 0; ind<4;ind++) {
+            int nexti = i + di[ind]; 
+            int nextj = j + dj[ind]; 
+            if(nexti >= 0 && nextj >= 0 && nexti < n && nextj < n && a[nexti][nextj] == 1) {
+                a[i][j] = 0;
+                move.push_back(dir[ind]);
+                solve(nexti, nextj, a, n, ans, move,di, dj,dir);
+                move.pop_back();
+                a[i][j] = 1; 
+            }
+        }
+    }
+    public:
+    vector<string> findPath(vector<vector<int>> &m, int n) {
+        vector<string> ans;
+        string dir = "DLRU";
+        int di[] = {+1, 0,  0,  -1}; 
+        int dj[] = {0, -1,  1,   0}; 
+        //			D	L	R	 U
+        string move="";
+        if(m[0][0] == 1) solve(0,0,m,n, ans,move,di, dj,dir); 
+        return ans; 
     }
 };
