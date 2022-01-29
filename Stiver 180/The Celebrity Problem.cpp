319c9ec1cbf9d80f9e839c85d178ca0ad997e7c2
shwetank singh <shwetanknaveen@gmail.com>
2022-01-29 22:19:10 +0530

    other approaches of celebrity problem

--- Stiver 180/The Celebrity Problem.cpp
+++ Stiver 180/The Celebrity Problem.cpp
@@ -83,3 +83,64 @@ public:
         return ans;
     }
 };
+
+//Approach 2 Time complexity = O(n) 	Space complexity = O(n)
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        deque<int> deq;
+       for(int i=0;i<n;i++)//initially everyone is potential celebrity
+           deq.push_back(i);
+        
+        int potentialCeleb;
+        while(!deq.empty())
+        {
+            if(deq.size()==1)//if only one person is remaining then he is potential celebrity
+            {
+                potentialCeleb = deq.front();
+                break;
+            }
+            
+            if(M[deq.front()][deq.back()])//if front guy know back guy then front guy can't be potential celebrity
+                deq.pop_front();
+            else if(M[deq.back()][deq.front()])//if back guy know front guy then he can't be potential celbrity
+                deq.pop_back();
+            else//both don't know each other hence both can't be celebrity
+            {
+                deq.pop_front();
+                deq.pop_back();//we have already insured that there are more than 1 elements on the deque
+            }
+        }
+        for(int i=0;i<n;i++)
+            if(i!=potentialCeleb && (M[potentialCeleb][i] || !M[i][potentialCeleb]))//if the last remaining guy knows someone or not known by someone then there is no celebrity at all
+                return -1;
+        
+        return potentialCeleb;
+    }
+};
+
+//Approach 3 => Utilising the approach 2
+//Time complexity = O(n)	Space complexity = O(1)
+
+class Solution 
+{
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        int potentialCeleb = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(M[potentialCeleb][i])
+                potentialCeleb = i;//earlier potentialCeleb knows i hence he can't be celebrity but  i can be which will be checked further
+        }
+        for(int i=0;i<n;i++)
+            if(i!=potentialCeleb && (M[potentialCeleb][i] || !M[i][potentialCeleb]))//if the last remaining guy knows someone or not known by someone then there is no celebrity at all
+                return -1;
+        
+        return potentialCeleb;
+    }
+};
