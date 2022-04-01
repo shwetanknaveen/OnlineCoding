f70dc67ca8f7d57e19b878a39441377ae1413700
shwetank singh <shwetanknaveen@gmail.com>
2022-04-01 16:48:37 +0530

    another method LIS

--- Stiver 180/Longest Increasing Subsequence.cpp
+++ Stiver 180/Longest Increasing Subsequence.cpp
@@ -1,5 +1,7 @@
 //Problem link - https://leetcode.com/problems/longest-increasing-subsequence/
 
+//Approach 1
+
 //If we are looking from right to left then we will have to find a strictly decreasing sequence. Initially we put INT_MAX on 
 //the right most side so that it is the first picked element and while comparison for numbers on left hand side, every no.
 //will be smaller than it and hence no issue in picking them.
@@ -32,5 +34,39 @@ public:
         vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),-1));
         return helper(nums,dp,nums.size()-1,nums.size()-2);//Initially the prevValInd is of INT_MAX which we push ourselve
         													//and we start looking from right most number in actual nums
+    }
+};
+
+//Approach 2
+//Since we have to find strictly increasing sequence hence we take sorted array which consists of unique elements from nums
+//in sorted manner without duplicates and then find length of longest common subsequence of this sorted array and nums
+
+class Solution {
+public:
+    int lcs(vector<int> &nums,vector<int> &sorted,int ind1,int ind2,vector<vector<int>> &dp)
+    {
+        if(ind1>=nums.size() || ind2>=sorted.size())
+            return 0;
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        
+        if(nums[ind1] == sorted[ind2])
+            dp[ind1][ind2] = 1+lcs(nums,sorted,ind1+1,ind2+1,dp);
+        else
+            dp[ind1][ind2] = max(lcs(nums,sorted,ind1+1,ind2,dp),lcs(nums,sorted,ind1,ind2+1,dp));
+        
+        return dp[ind1][ind2];
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        set<int> uniqNums;
+        for(int i:nums)
+            uniqNums.insert(i);//set will push the nums in sorted order automatically
+        vector<int> sorted;
+        set<int>::iterator itr;
+        for(itr = uniqNums.begin();itr!=uniqNums.end();itr++)
+        {
+            sorted.push_back(*itr);
+        }
+        vector<vector<int>> dp(nums.size(),vector<int>(sorted.size(),-1));
+        return lcs(nums,sorted,0,0,dp);
     }
 };
