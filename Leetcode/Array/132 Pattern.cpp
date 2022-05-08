09a835a5904e82c6dbccf167f86c920ddad5313c
shwetank singh <shwetanknaveen@gmail.com>
2022-05-08 22:21:19 +0530

    flatten nested list iterator

--- Leetcode/Array/132 Pattern.cpp
+++ Leetcode/Array/132 Pattern.cpp
@@ -1,5 +1,5 @@
 //Problem link - https://leetcode.com/problems/132-pattern/
-
+//Self video explanation - https://www.youtube.com/watch?v=3fQORixMtd8&list=PLudlC33BR8eEiSWH654z2GKEF3Ckz6QWB&index=4
 //Apporach 1 -> Brute force -> Gives TLE
 
 class Solution {
@@ -36,8 +36,9 @@ public:
 											//j. Hence to follow the condition that number at this index is smaller than smaller of the already
 											//found pair is that the smaller one the found pair is just smaller and not the smallest.
 											//And that is what we found -> Number at k was just smaller than number at j
+											
             else while( !st.empty() && nums[i] > st.top())//We will drop from stack if the number on stack is smaller than this number
-			{ 
+			{ 											//We won't update atK even if the number on stack is equal to this number
               atK = st.top();//for this index, keep the number on right side which is JUST smaller than this number in atK variable 
 			  st.pop(); 
             }
