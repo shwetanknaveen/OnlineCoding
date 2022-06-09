a5cee5de1a3c35b61210a6e8c3eb35e2835b1753
Shwetank Singh <shwetanknaveen@gmail.com>
2022-06-09 16:29:58 +0530

    two game questions

---
+++ Leetcode/Dynamic programming/Stone Game.cpp
@@ -0,0 +1,8 @@
+//Problem link - https://leetcode.com/problems/stone-game/
+//See "Leetcode/Dynamic Programming/Predict the Winner"
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
