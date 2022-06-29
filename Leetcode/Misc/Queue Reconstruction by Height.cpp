14c0ef7b7f4d19d18fe48515b89c0044611b0009
Shwetank Singh <shwetanknaveen@gmail.com>
2022-06-30 01:14:17 +0530

    one more approach

--- Leetcode/Misc/Queue Reconstruction by Height.cpp
+++ Leetcode/Misc/Queue Reconstruction by Height.cpp
@@ -1,5 +1,33 @@
 //Problem link - https://leetcode.com/problems/queue-reconstruction-by-height/
 
+//Approach 1
+/*
+To satisfy the condition of rank for any person, all the taller persons and all the persons having same height having
+lower rank (second index value is smaller) should be already present in the queue so that this person can be inserted at the right position. For e.g.,tallest person having second index value as 0 should be inserted first because if 
+some other person having same height but second value as 1 comes, then it can be inserted behind the tallest person
+having value 0
+*/
+
+class Solution {
+public:
+    static bool cmp(vector<int> &v1,vector<int> &v2)
+    {
+        if(v1[0]>v2[0]) return true;//first sort as per descending order of height
+        else if(v1[0]==v2[0]) return v1[1]<v2[1];//if height is same then sort as per asceding value of second index
+        return false;
+    }
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(),people.end(),cmp);
+        vector<vector<int>> ans;
+        for(auto v:people)
+            ans.insert(ans.begin()+v[1],v);//insert as per the second index value of this person because all the
+                                            //persons having taller height or persons having equal height having
+                                            //lower second index value would already been inserted hence this person
+                                            //will automatically get inserted at the right place
+        return ans;
+    }
+};
+//Approach 2
 class Solution {
 public:
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) 
